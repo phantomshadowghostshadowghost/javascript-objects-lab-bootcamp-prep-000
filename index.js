1296422d219480584d0a81255c10066147b4edfc
Lamido Tijjani <kinglamido@gmail.com>
2018-04-07 21:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function Objects() {
-  return recipes
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var lmd = delete object[key]
+  return lmd
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+ var newObj = object.assign({}, obj)
 }
