62f3e61a48df85dadb080091de5b87f41cdc7306
Lamido Tijjani <kinglamido@gmail.com>
2018-04-07 21:53:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var lmd= object
+  var lmd = Object.assign({}, object, {[key]: value})
   return delete lmd[key]
 }
 
